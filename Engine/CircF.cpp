cdac3893132b800940a8e6873e8488b7bfe6297d
SunTroll <50963945+Sun-Troll@users.noreply.github.com>
2020-04-05 21:38:37 +0200

    ContainsCenter chaged to ContainsPoint

--- Engine/CircF.cpp
+++ Engine/CircF.cpp
@@ -12,12 +12,12 @@ bool CircF::Coliding(const CircF& circ) const
 	return (pos - circ.pos).GetLengthSq() < (radius + circ.radius) * (radius + circ.radius);
 }
 
-bool CircF::ContainsCenter(const CircF & circ) const
-{
-	return (pos - circ.pos).GetLengthSq() < radius * radius;
-}
-
 bool CircF::ContainsCirc(const CircF& circ) const
 {
 	return (pos - circ.pos).GetLength() + circ.radius < radius;
+}
+
+bool CircF::ContainsPoint(const VecF& point) const
+{
+	return (pos - point).GetLengthSq() < radius * radius;
 }
--- Engine/CircF.h
+++ Engine/CircF.h
@@ -7,8 +7,8 @@ class CircF
 public:
 	CircF(const VecF& pos, float radius);
 	bool Coliding(const CircF& circ) const;
-	bool ContainsCenter(const CircF& circ) const;
 	bool ContainsCirc(const CircF& circ) const;
+	bool ContainsPoint(const VecF& point) const;
 public:
 	VecF pos;
 	float radius;
