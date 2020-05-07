71abbaf81a65517518fb76c65da5a0c611f79311
SunTroll <50963945+Sun-Troll@users.noreply.github.com>
2020-05-07 21:22:42 +0200

    lag comment

--- Engine/Player.h
+++ Engine/Player.h
@@ -80,7 +80,7 @@ private:
 	float hpMax = hpBase;
 	float hpCur = hpMax;
 	static constexpr float radius = 5.0f;
-	static constexpr float baseFireTimePlayerAnim = 0.12f;
+	static constexpr float baseFireTimePlayerAnim = 0.12f; // starts lagging at 0.001f
 	float maxFireTimePlayerAnim = baseFireTimePlayerAnim;
 	float curFireBasePlayerAnim = 0.0f;
 	static constexpr float drawDamageTimeMax = 0.2f;
